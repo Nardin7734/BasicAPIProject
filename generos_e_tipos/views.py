e2f32f05dc745193ff3899f0b3274abca7e4267b
Diego <ddnardin@gmail.com>
2021-09-15 00:20:12 -0300

    refinando permissões e autenticações

--- generos_e_tipos/views.py
+++ generos_e_tipos/views.py
@@ -2,12 +2,14 @@ from rest_framework import viewsets
 from .serializers import GenerosSerializer, TiposSerializer
 from .models import Generos, Tipos
 from rest_framework.permissions import IsAuthenticated, DjangoModelPermissions
+from rest_framework.authentication import BasicAuthentication, SessionAuthentication
 
 
 class GenerosViewSet(viewsets.ModelViewSet):
     queryset = Generos.objects.all()
     serializer_class = GenerosSerializer
     permission_classes = [IsAuthenticated, DjangoModelPermissions]
+    authentication_classes = [BasicAuthentication, SessionAuthentication]
 
 
 
@@ -15,3 +17,5 @@ class TiposViewSet(viewsets.ModelViewSet):
     queryset = Tipos.objects.all()
     serializer_class = TiposSerializer
     permission_classes = [IsAuthenticated, DjangoModelPermissions]
+    authentication_classes = [BasicAuthentication, SessionAuthentication]
+
