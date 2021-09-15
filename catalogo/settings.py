d826913a4298c253405a25b9444cc636ffcdce58
Diego <ddnardin@gmail.com>
2021-09-15 00:42:00 -0300

    removendo a key

--- catalogo/settings.py
+++ catalogo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&gc+u*q3hgalw-aixck1m-%0=mlmy-mt&%kgyj!n(g2v-33uel'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
